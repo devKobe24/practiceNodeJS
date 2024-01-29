3ad17cb84f8659db908abf7b652dae85dfbaf5f9
kobe <dev.skyachieve91@gmail.com>
2024-01-29 17:13:02 +0900

    :sparkles: Feat: index.html 랜더링하여 웹서버 라우터에 라우팅.

--- express/server.js
+++ express/server.js
@@ -5,8 +5,12 @@ const server = app.listen(3000, () => {
   console.log('Start Server : localhost:3000');
 });
 
+app.set('views', '../views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
 app.get('/', function(req, res) {
-  res.send('Hello, World')
+  res.render('index.html')
 });
 
 app.get('/about', function(req, res) {
